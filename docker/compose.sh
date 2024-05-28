ada3a247aa7eb9de9ca1e3f1a368bb92e7bbee6a
pdh <pdh930105@gmail.com>
2024-05-28 23:06:20 +0900

    udpate docker

---
+++ docker/compose.sh
@@ -0,0 +1,17 @@
+export USERID=$(id -u)
+export GROUPID=$(id -g)
+export USERNAME=$(whoami)
+export CONTAINERNAME=${1:-idslab_reparam}
+export COMPOSE_PROJECT_NAME=${2:-idslab_reparam}
+#export CONTAINERNAME=${CONTAINERNAME:-pdh_bq_pt_2210}
+printf "USERID=%s\n" $USERID 
+printf "GROUPID=%s\n" $GROUPID 
+printf "USERNAME=%s\n" $USERNAME
+printf "CONTAINERNAME=%s\n" $CONTAINERNAME 
+xhost +
+xhost +local:docker
+echo $xhost
+docker compose down
+docker compose up -d --build --remove-orphans
+docker compose exec echo $xhost
+docker exec -it $CONTAINERNAME /bin/bash
